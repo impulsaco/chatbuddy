e370d0288f83446c1a9e337dcc41ff9111a59d7a
Diego Salvatierra <diego@macbook-air-4.lan>
2023-03-09 23:17:39 -0800

    UX improvements

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,9 +1,12 @@
-import { View, StyleSheet, Dimensions, Text} from 'react-native';
+import { View, StyleSheet, Dimensions, Text, TouchableOpacity} from 'react-native';
 import React, { useState, useEffect} from 'react';
 import 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient'
 import { Button } from '@rneui/themed'
 import { supabase } from '../lib/supabase';
+import KoreanFlag from '../../assets/KoreanFlag.svg';
+import SpanishFlag from '../../assets/SpanishFlag.svg';
+import BulgarianFlag from '../../assets/BulgarianFlag.svg';
 
 const PAGE_HEIGHT = Dimensions.get('window').height;
 
@@ -23,6 +26,14 @@ function Home({navigation}) {
       setSession(session)
     })
 
+    /*const LanguageBox = ({lang, langCode, flag}) => {
+        return (
+            <TouchableOpacity style={styles.languageBox} onPress={() => navigation.navigate('Choose', {lang: lang, langCode: langCode})}>
+                <Text style={styles.buttonText}>{lang}</Text>
+                {flag}
+            </TouchableOpacity>
+        )
+    }*/
 
     const renderButtons = () => { 
       
@@ -38,18 +49,30 @@ function Home({navigation}) {
           return (
             <View>
                 <View style={styles.textContainer}>
-                  <Text style={styles.mainText}>Choose a language</Text>
+                  <Text style={styles.mainText}>What would you like to learn?</Text>
                 </View>     
                 <View style={styles.buttonsContainer}>
                   <View style={styles.buttonContainer}>
-                      <Button buttonStyle={{ backgroundColor: '#FFC107', marginRight: 30}} onPress={() => navigation.navigate('Choose', {lang: "Korean", langCode: "ko"})}>Korean</Button>            
-                      <Button buttonStyle={{ backgroundColor: '#FFC107' }} onPress={() => navigation.navigate('Choose', {lang: "Spanish", langCode: "es-MX"})}>Spanish</Button>
+                    <TouchableOpacity style={styles.languageBox} onPress={() => navigation.navigate('Choose', {lang: "Korean", langCode: "ko"})}>
+                      <Text style={styles.buttonText}>Korean</Text>
+                      <KoreanFlag width={55}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.languageBox} onPress={() => navigation.navigate('Choose', {lang: "Spanish", langCode: "es-MX"})}>
+                      <Text style={styles.buttonText}>Spanish</Text>
+                      <SpanishFlag width={55}/>
+                    </TouchableOpacity>                      
                   </View>
-                  <View style={styles.phrasebookContainer}>
-                      <Button buttonStyle={{ backgroundColor: '#FFC107', width: 120}} onPress={() => navigation.navigate('Phrasebook')}>Phrasebook</Button>
-                  </View>
-                  <View style={styles.logOutContainer}>
-                      <Button buttonStyle={{ backgroundColor: '#FFC107' }} onPress={() => supabase.auth.signOut()}>Log Out</Button>
+                  <View style={styles.lowerContainer}> 
+                    <View style={styles.phrasebookContainer}>                    
+                        <TouchableOpacity style={styles.phrasebookButton} onPress={() => navigation.navigate('Phrasebook')}>
+                          <Text style={styles.buttonText}>My phrases</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.logOutContainer}>
+                        <TouchableOpacity style={styles.logoutButton} onPress={() => supabase.auth.signOut()}>
+                          <Text style={[styles.buttonText, {color: "white"}]}>Log out</Text>
+                        </TouchableOpacity>                      
+                    </View>
                   </View>
                 </View>
             </View>
@@ -90,6 +113,57 @@ function Home({navigation}) {
       justifyContent: 'center', 
       padding: 10
     },
+    lowerContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+    },
+    buttonText: {
+      fontSize: 16,
+      color: "black"
+    },            
+    languageBox: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 10,
+      width: PAGE_WIDTH*.2,
+      height: PAGE_HEIGHT*.1,
+      margin: 10
+    },
+    phrasebookButton: {
+      display: 'flex',
+      flex: 2,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 10,
+      width: PAGE_WIDTH*.45,
+      height: PAGE_HEIGHT*.05,
+      margin: 10
+    },
+    logoutButton: {
+      display: 'flex',
+      flex: 2,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+      borderColor: 'white',
+      borderWidth: 1,
+      borderRadius: 10,
+      width: PAGE_WIDTH*.45,
+      height: PAGE_HEIGHT*.05,
+      margin: 10
+    },
+
     bottomContainer: {
       display: 'flex',
       padding: 10,
@@ -103,11 +177,12 @@ function Home({navigation}) {
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
-      width: PAGE_WIDTH,
+      width: PAGE_WIDTH*.8,
     },
     mainText: {
-      fontSize: 54,
+      fontSize: 36,
       color: "white",
+      textAlign: 'center',
      },
     buttonsContainer: {
       display: 'flex',
@@ -128,14 +203,12 @@ function Home({navigation}) {
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: 'row',
-      padding: 10
     },
     logOutContainer: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: 'row',
-      padding: 10
     },      
   })
 
