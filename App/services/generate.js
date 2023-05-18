f9c7991c0647914e1bc9b575436a5a6bc0952e34
marrowxiong@gmail.com <lipingxiong@apples-MacBook-Pro.local>
2023-05-18 14:08:23 +0800

    move api/ services/

---
+++ App/services/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+import "react-native-url-polyfill/auto";
+
+console.log("entered API");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  console.log("ENTERING API");
+  console.log("REQUEST IS ", req.body.sentence);
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: generatePrompt(req.body.sentence),
+    temperature: 0.6,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+  console.log("returned data is ", completion.data.choices[0].text);
+}
+
+function generatePrompt(sentence) {
+  return `Correct the following Spanish sentence: ${sentence}`;
+}
---
+++ App/services/romanizer.js
@@ -0,0 +1,49 @@
+import { REACT_APP_SERVER_URL } from "@env";
+import axios from "axios";
+import "react-native-url-polyfill/auto";
+import { supabase } from "@app/lib/supabase";
+
+// Romanize sentence
+
+const romanizer = (input, setSentenceRomanized, lang, langCode, session) => {
+  const romanizeSentence = async (input, lang, langCode) => {
+    if (langCode === "ko" || langCode === "bg" || langCode === "ja") {
+      const prompt = `Romanize the following sentence in  ${lang}: ${input}. Output only the romanized sentence`;
+      const role = `Simple ${lang} romanizer.`;
+      const costPerToken = 0.002 / 1000;
+      let sentenceRomanizedTemp = "";
+
+      const serverUrl = REACT_APP_SERVER_URL;
+      // Send call to node.js
+      const response = await axios.post(`${serverUrl}/api/gpt-completions`, {
+        role: role,
+        prompt: prompt,
+      });
+      sentenceRomanizedTemp = response.data.choices[0].message.content.trim();
+      setSentenceRomanized(sentenceRomanizedTemp);
+
+      // Save call to Supabase
+      const saveCall = async () => {
+        const { error } = await supabase.from("aiUsage").insert({
+          created_at: new Date().toISOString(),
+          user: session.user.id,
+          model: "gpt-3.5-turbo",
+          type: "romanizer",
+          chars: role.length + prompt.length,
+          tokens: (role.length + prompt.length) / 4,
+          seconds: null,
+          cost: ((role.length + prompt.length) / 4) * costPerToken,
+          prompt: `${role} ${prompt}`,
+          output: sentenceRomanizedTemp,
+          api_key: null,
+        });
+        if (error) alert(error.message);
+      };
+      saveCall();
+    }
+  };
+
+  romanizeSentence(input, lang, langCode);
+};
+
+export default romanizer;
